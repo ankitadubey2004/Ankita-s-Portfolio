c1440b9c44d70f66fdc04ba5ca0a35337d136a1f
Ankita Dubey <155213532+ankitadubey2004@users.noreply.github.com>
2024-07-08 00:42:11 +0530

    push

--- main.js
+++ main.js
@@ -45,4 +45,13 @@ ScrollReveal({
 ScrollReveal().reveal('.home-content,heading', { origin:'top'});
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin : 'bottom' });
 ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left' });
-ScrollReveal().reveal('.home-contact p', { delay: 500 });
+ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right' });
+
+//typed js
+const typed = new Typed('.multiole-text', {
+    strings: ['Web Developer','Java Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop:true,
+});
